48f302043fa836f0b48d139317c7a7c79941f834
doradorani <sokoyo2@gmail.com>
2022-01-24 17:21:31 +0900

    정보 수정, 탈퇴

--- aivle/accounts/views.py
+++ aivle/accounts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
-from .forms import UserForm
+from .forms import CustomUserForm, UserChangeForm
 from django.contrib.auth import authenticate, login
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
 
 
 # Create your views here.
@@ -11,7 +13,7 @@ from django.contrib.auth.models import User
 @csrf_exempt
 def register(request):
     if request.method == "POST":
-        form = UserForm(request.POST)
+        form = CustomUserForm(request.POST)
         if form.is_valid():
             form.save()
 
@@ -23,5 +25,31 @@ def register(request):
 
             return redirect('accounts:login')
     else:
-        form = UserForm()
+        form = CustomUserForm()
     return render(request, 'register/register.html', {'form' : form })
+
+
+@login_required
+def update(request):
+    if request.method == "POST": #Post -> 수정한 내용 업데이트, Get -> 기존 데이터 instance 담아서 폼을 html넘김
+        form = UserChangeForm(request.POST, instance = request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('upadate/update_complete.html', request.user)
+        return render(
+            request, 
+            'update/update.html', 
+            {'form': form }
+            )
+
+@login_required
+def delete(request):
+    if request.method == 'POST':
+        request.user.delete()
+        return redirect('posts:list')
+    return render(request, 'delete/delete.html')
+
+
+
+
+       
