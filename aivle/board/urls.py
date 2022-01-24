ba1e7d222169533f4d8ab4b7a0821b4bc0ad17eb
monat96 <42468323+monat96@users.noreply.github.com>
2022-01-24 15:46:09 +0900

    add board

--- aivle/board/urls.py
+++ aivle/board/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'board'
 urlpatterns = [
     path('main/', views.main, name='main'),
+    path('board/', views.board, name='board'),
 ]
--- aivle/board/views.py
+++ aivle/board/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def main(request):
-    return render(request, 'main/main.html')
+    return render(request, 'board/main.html')
+def board(request):
+    return render(request, 'board/board.html')
