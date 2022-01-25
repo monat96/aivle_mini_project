c42a4911f217749f6837f2a988791293686a7739
doradorani <sokoyo2@gmail.com>
2022-01-25 11:01:30 +0900

    board_model

--- aivle/board/models.py
+++ aivle/board/models.py
@@ -1,46 +1,44 @@
 from django.db import models
 from django.db.models.fields import CharField, IntegerField, DateTimeField, FloatField
 from django.utils.timezone import now
+from django.conf import settings
 
 
-# Create your models here.
+class Board(models.Model):
+    board_id = models.IntegerField(primary_key=True)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name = "brduser_id")
+    title = models.CharField(max_length= 45)
+    content = models.CharField(max_length= 400)
+    date = models.DateTimeField(default=now, editable=False)
+    like = models.IntegerField()
+    dislike = models.IntegerField()
+    hit_cnt = models.IntegerField()
 
-# class Board(models.Model):
-#     board_id = models.IntegerField(primary_key=True)
-#     user_id = models.ForeignKey('accounts.CustomUser', related_name='username', on_delete=models.CASCADE)
-#     title = models.CharField(max_length= 45)
-#     content = models.CharField(max_length= 400)
-#     date = models.DateTimeField(default=now, editable=False)
-#     like = models.IntegerField()
-#     dislike = models.IntegerField()
-#     hit_cnt = models.IntegerField()
-
-#     class Meta:
-#         db_table = 'board'
+    class Meta:
+        db_table = 'board'
 
 
 
 
-# class Comment(models.Model):
-#     comment_num = models.IntegerField(primary_key=True)
-#     board_id = models.ForeignKey(Board, on_delete=models.CASCADE)
-#     user_id = models.ForeignKey('accounts.CustomUser', related_name='username', on_delete=models.CASCADE)
-#     user_nickname = models.ForeignKey('accounts.CustomUser', related_name='nickname', on_delete=models.CASCADE)
-#     content = models.CharField(max_length= 45)
-#     like = models.IntegerField()
-#     dislike = models.IntegerField()
+class Comment(models.Model):
+    comment_num = models.IntegerField(primary_key=True)
+    board_id = models.ForeignKey(Board, on_delete=models.CASCADE, verbose_name = "cmtboard_id")
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name = "cmtuser_id")
+    content = models.CharField(max_length= 45)
+    like = models.IntegerField()
+    dislike = models.IntegerField()
 
-#     class Meta:
-#         db_table = 'comment'
+    class Meta:
+        db_table = 'comment'
 
-# class File(models.Model):
-#     file_idx = models.IntegerField()
-#     board_id = models.ForeignKey(Board, on_delete=models.CASCADE)
-#     user_id = models.ForeignKey('accounts.CustomUser', related_name='user_id', on_delete=models.CASCADE)
-#     stored_file_name = models.CharField(max_length=45)
-#     file_size = models.IntegerField()
-#     create_date = models.DateTimeField(default=now, editable=False)
+class File(models.Model):
+    file_idx = models.IntegerField()
+    board_id = models.ForeignKey(Board, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name = "fleuser_id")
+    stored_file_name = models.CharField(max_length=45)
+    file_size = models.IntegerField()
+    create_date = models.DateTimeField(default=now, editable=False)
 
-#     class Meta:
-#         db_table = 'file'
+    class Meta:
+        db_table = 'file'
 
