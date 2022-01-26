bfd815d16d5ea51ebf3d91a81ce8b9776c27b758
monat96 <42468323+monat96@users.noreply.github.com>
2022-01-26 13:10:48 +0900

    [완료] 마이페이지 내가 쓴 글

--- aivle/board/urls.py
+++ aivle/board/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('board/', views.boardpaging, name='board'),
     # path('new/',views.new_topic,name='new_topic'),
     path('write/',views.board_write, name = 'write'), # 게시글 작성
+    path('mypage/', views.mypage, name='mypage'),
     # path('detail/<int:pk>/',views.boarddetail, name = 'board_detail'),
     # path('detail/<int:pk>/edit', views.boardedit, name = 'board_edit'),
     # path('detail/<int:pk>/delete',views.boarddelete,name='board_delete'),
