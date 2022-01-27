f37896375562d9e23dc414c772b1d8e8f8fbd28d
monat96 <42468323+monat96@users.noreply.github.com>
2022-01-27 21:14:03 +0900

    admin 모델 추가

--- aivle/board/admin.py
+++ aivle/board/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Notice
+from .models import Notice, Board
 
 
 admin.site.register(Notice)
+admin.site.register(Board)
 # Register your models here.
