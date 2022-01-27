f063661fca148edef8f0df83fedf8aef4355bba7
doradorani <sokoyo2@gmail.com>
2022-01-27 17:20:31 +0900

    notice_detail 수정

--- aivle/board/urls.py
+++ aivle/board/urls.py
@@ -17,6 +17,6 @@ urlpatterns = [
     # path('detail/<int:pk>/',views.boarddetail, name = 'board_detail'),
     # path('detail/<int:pk>/edit', views.boardedit, name = 'board_edit'),
     # path('detail/<int:pk>/delete',views.boarddelete,name='board_delete'),
-    # path('notice_detail/',views.notice_detail, name = 'notice_detail'),
+    path('notice_detail/<int:pk>/',views.notice_detail, name = 'notice_detail'),
     # path('detail/<int:pk>/commentwrite',views.comment_write, name = 'comment_write'),
 ]
