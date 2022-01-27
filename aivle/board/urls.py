79e45264617fa8b2b07e3da41933e98eba33506c
doradorani <sokoyo2@gmail.com>
2022-01-27 14:07:44 +0900

    메인 설명글 정중앙

--- aivle/board/urls.py
+++ aivle/board/urls.py
@@ -4,10 +4,8 @@ from . import views
 app_name = 'board'
 urlpatterns = [
     path('main/', views.main, name='main'),
-    #path('board/', views.board, name='board'),
     path('board/', views.boardpaging, name='board'),
     path('notice/', views.notice_boardpaging, name='notice'),
-    # path('new/',views.new_topic,name='new_topic'),
     path('write/',views.board_write, name = 'write'), # 게시글 작성
     path('detail/<int:pk>/',views.board_detail, name = 'board_detail'),
     path('download/', views.download, name='download'),
@@ -15,9 +13,10 @@ urlpatterns = [
     path('mypage/', views.mypage, name='mypage'),
     path('withdraw/', views.withdraw, name='withdraw'),
     path('change_password/', views.change_password, name='change_password'),
+    path('detail/<int:pk>/download',views.download(), name="download"),
     # path('detail/<int:pk>/',views.boarddetail, name = 'board_detail'),
     # path('detail/<int:pk>/edit', views.boardedit, name = 'board_edit'),
     # path('detail/<int:pk>/delete',views.boarddelete,name='board_delete'),
-    # path('notice',views.notice_detail, name = 'noticedetail'),
+    # path('notice_detail/',views.notice_detail, name = 'notice_detail'),
     # path('detail/<int:pk>/commentwrite',views.comment_write, name = 'comment_write'),
 ]
